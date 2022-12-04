57279dbd50f628a4004862165ce02d06dff9fc3b
SerhiiCho <serhiicho@protonmail.com>
2022-12-04 21:35:15 +0200

    Added home page

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,23 +3,32 @@ import { defineUserConfig, defaultTheme } from 'vuepress'
 export default defineUserConfig({
     lang: 'en-US',
     title: 'Timeago',
-    description: 'Fast and lightweight date time package that converts given date into "n time ago" format',
+    description: 'Fast and lightweight date time package for Go',
     head: [
         ['link', { rel: 'icon', href: '/images/favicon.png' }]
     ],
     theme: defaultTheme({
+        logo: '/images/logo.png',
         navbar: [
+            {
+                text: 'Documentation',
+                link: '/documentation',
+            },
+            {
+                text: 'Usage example',
+                link: 'https://replit.com/@SerhiiCho/Usage-of-timeago-package',
+            },
             {
                 text: 'GitHub',
                 link: 'https://github.com/SerhiiCho/timeago',
             },
             {
-                text: 'Author',
-                link: 'https://serhii.io',
+                text: 'Changelog',
+                link: 'https://github.com/SerhiiCho/timeago/blob/master/docs/CHANGELOG.md',
             },
             {
-                text: 'More packages',
-                link: 'https://serhii.io/projects',
+                text: 'About author',
+                link: 'https://serhii.io/about-me',
             },
         ],
     }),
