a500fe8caa86f11356f4c1da307b1e205d02b4fb
SerhiiCho <serhiicho@protonmail.com>
2022-12-04 19:19:43 +0200

    Added favicon

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,13 +5,21 @@ export default defineUserConfig({
     title: 'Timeago',
     description: 'Fast and lightweight date time package that converts given date into "n time ago" format',
     head: [
-        // ['link', { rel: 'icon', href: '/images/logo.png' }]
+        ['link', { rel: 'icon', href: '/images/favicon.png' }]
     ],
     theme: defaultTheme({
         navbar: [
             {
                 text: 'GitHub',
                 link: 'https://github.com/SerhiiCho/timeago',
+            },
+            {
+                text: 'Author',
+                link: 'https://serhii.io',
+            },
+            {
+                text: 'More packages',
+                link: 'https://serhii.io/projects',
             },
         ],
     }),
